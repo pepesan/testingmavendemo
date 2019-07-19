bfbda9a6234815cd9ef72919e516280847c063b6
pepesan <pepesan@gmailcom>
2019-07-19 18:06:52 +0200

    arreglado seguro!!!

--- src/test/java/com/cursosdedesarrollo/TestCalculadora.java
+++ src/test/java/com/cursosdedesarrollo/TestCalculadora.java
@@ -16,19 +16,19 @@ public class TestCalculadora {
     @Test
     public void testPropiedad(){
         Calculadora c = new Calculadora();
-        assertEquals(new Integer(2),c.dato);
+        assertEquals(new Integer(2),c.getDato());
     }
 
     @Test
     public void testContructorParams(){
         Integer i = 2;
         Calculadora c = new Calculadora(2);
-        assertEquals(i,c.dato);
+        assertEquals(i,c.getDato());
     }
     @Test
     public void testContructorParamsWrongData(){
         Integer i = 2;
         Calculadora c = new Calculadora(2);
-        assertNotEquals(new Integer(3),c.dato);
+        assertNotEquals(new Integer(3),c.getDato());
     }
 }
